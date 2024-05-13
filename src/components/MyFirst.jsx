c9165224fdf4ef60bfe5964c2a754cb0ad65494e
Waad-Raggam <108654364+Waad-Raggam@users.noreply.github.com>
2024-05-13 20:36:20 +0300

    states and setstate

--- src/components/MyFirst.jsx
+++ src/components/MyFirst.jsx
@@ -1,3 +1,5 @@
+// dont forget imports when using state
+import { useState } from "react";
 const MyFirst = () => {
     // script
     const name = "Welcome to tuwaiq";
@@ -7,6 +9,26 @@ const MyFirst = () => {
     const ShowMessage=()=>{
         alert("you clicked button");
     }
+
+    // states in react
+    let nameState = "use state";
+    const ChangeNameState=()=>{
+        nameState="changed";
+
+    }
+    // below is wrong
+    //  const [tripData, setTripState] = useState('the trip is next week');
+    //  const ChangeTripData=()=>{
+    //     tripData = "lol cancelled";
+    // }
+     // State
+     const [tripData, setTripData] = useState('the trip is next week');
+
+     // State update function
+     const changeTripData = () => {
+         setTripData("lol cancelled");
+     };
+ 
     return(
         //html
         <> 
@@ -15,13 +37,18 @@ const MyFirst = () => {
          <p>Welcome to my first component</p>
         <p>{name}</p>
         <p>{x}</p>
-
         {/* events in jsx */}
         {/* <button onClick={()=>alert("you clicked button")}>Click</button> */}
 
         {/* instead of writing function inside jsx like line 20, declare function ShowMessage and use it as follows */}
 
         <button onClick={ShowMessage}>Click</button>
+
+
+        {/* state */}
+
+        <p>{tripData}</p>
+        <button onClick={changeTripData}>Click</button>
         </>
 
     );
