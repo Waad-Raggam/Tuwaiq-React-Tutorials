ea92dbd73b3dae707cb5a37bc168d21cec789a28
Waad-Raggam <108654364+Waad-Raggam@users.noreply.github.com>
2024-05-13 19:55:04 +0300

    variables in components

--- src/components/MyFirst.jsx
+++ src/components/MyFirst.jsx
@@ -1,9 +1,16 @@
 const MyFirst = () => {
     // script
+    const name = "Welcome to tuwaiq";
+    const x = 10.5
 
     return(
         //html
-        <p>Welcome to my first component</p>
+        <>
+         <p>Welcome to my first component</p>
+        <p>{name}</p>
+        <p>{x}</p>
+        </>
+
     );
 }
 
