4de532eb17e98b9b6c491447d5c5d929b4371d36
Waad-Raggam <108654364+Waad-Raggam@users.noreply.github.com>
2024-05-14 20:28:21 +0300

    sum calculator - his way

--- src/components/SumCalculator.jsx
+++ src/components/SumCalculator.jsx
@@ -1,21 +1,33 @@
 import React, { useState } from "react";
 
 function SumCalculator() {
-    const [num1, setNum1] = useState("");
-    const [num2, setNum2] = useState("");
+    const [num1, setNum1] = useState(0);
+    const [num2, setNum2] = useState(0);
+    const [val, setVal] = useState(0);
+    const [sum, setSum] = useState(0);
 
-    const handleChangeNum1 = (e) => {
-        setNum1(e.target.value);
-    };
+    const GetVal=(e, Setnum)=>{
+        Setnum(e.target.value);
+    }
 
-    const handleChangeNum2 = (e) => {
-        setNum2(e.target.value);
-    };
+    // const handleNumberChange = (e, set)
 
-    const calculateSum = () => {
-        const sum = parseFloat(num1) + parseFloat(num2);
-        console.log("Sum=", sum);
-    };
+    // const handleChangeNum1 = (e) => {
+    //     setNum1(e.target.value);
+    // };
+
+    // const handleChangeNum2 = (e) => {
+    //     setNum2(e.target.value);
+    // };
+
+    // const calculateSum = () => {
+    //     const sum = parseFloat(num1) + parseFloat(num2);
+    //     console.log("Sum=", sum);
+    // };
+
+    const Summation =()=>{
+        setSum(num1+num2);
+    }
 
     return (
         <div>
@@ -23,16 +35,21 @@ function SumCalculator() {
             <input 
                 type="text" 
                 value={num1} 
-                onChange={handleChangeNum1} 
+                onChange={(e)=>GetVal(e, setNum1)} 
                 placeholder="First number" 
+                id = "num1"
+                name="num1"
             />
             <input 
                 type="text" 
                 value={num2} 
-                onChange={handleChangeNum2} 
+                onChange={(e)=>GetVal(e,setNum2)} 
                 placeholder="Second number" 
+                id="num2"
+                name="num2"
             />
-            <button onClick={calculateSum}>Calculate</button>
+            <button onClick={Summation}>Calculate</button>
+            <h1>{num1}+{num2}={sum}</h1>
         </div>
     );
 }
